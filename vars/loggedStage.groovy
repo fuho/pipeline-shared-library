53c42083ce759d4d7beb9e6b49ed6a2dd11c311b
Ondrej Dolejsi <ondrej.dolejsi@gmail.com>
2017-11-09 23:44:24 -0500

    better naming

--- vars/loggedStage.groovy
+++ vars/loggedStage.groovy
@@ -1,14 +1,14 @@
 /**
  * Created by odolejsi on 11/9/17.
  */
-def call(name, body) {
+def call(stageName, body) {
 
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-    stage{
-        echo "Start executing stage of name: $name"
+    stage(stageName){
+        echo "Start executing stage of name: $stageName"
         body()
-        echo "Done executing stage of name: $name"
+        echo "Done executing stage of name: $stageName"
     }
 }
